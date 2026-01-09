f6707c2e950ca928635caa88af46cb5b2111c468
canduki21 <cacndela@gmail.com>
2026-01-09 17:19:58 -0500

    Update DW2_MAIN.py

--- DW2_MAIN.py
+++ DW2_MAIN.py
@@ -5,12 +5,6 @@ import time
 dw2S.dw2_sens_setup()
 time.sleep(1)
 
-dw2S.dw2_sens_read()
-
-print("DW2: writing thermal data")
-dw2F.dw2_file_therm_write()
-
-print("DW2: repeated logging")
 for i in range(4):
-    time.sleep(5)
     dw2F.dw2_file_write()
+    time.sleep(5)
